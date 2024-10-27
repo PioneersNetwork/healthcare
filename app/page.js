4b25e817dfa7a97b63724399bf5dd63d3bcf019b
PioneersNetwork <33698879+PioneersNetwork@users.noreply.github.com>
2024-10-27 04:00:42 +0300

    0.0.001

--- app/page.js
+++ app/page.js
@@ -1,101 +1,111 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
 
 export default function Home() {
+  const [patient,setPatient]=useState({name:"",file_id:"",dob:"",gender:"",hypnosis:"",donte:"",history:"",medicen:"",exitDate:"",exitTime:""})
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <form>
+    <div className="container mx-auto p-[16px]">
+      <div className="flex justify-start">
+        <img className="w-[82px]" src="/logo.png" />
+      </div>
+      <div className="mx-auto ">
+        <img className="mx-auto pt-[16px]" src="/headerimage.png" />
+      </div>
+      <div className="justify-start">
+        <label className="mb-[8px]">الاسم المريض:</label>
+        <input
+        required
+          className="border h-[48px]  bg-slate-100 w-full p-[4px] rounded"
+          placeholder="اسم المريض"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      </div>
+      <div className="justify-start mt-[16px]">
+        <label className="mb-[8px]">رقم الملف:</label>
+        <input
+        required
+          type="number"
+          className="border h-[48px]  bg-slate-100 w-full p-[4px] rounded"
+          placeholder="رقم الملف"
+        />
+      </div>
+      <div className="justify-start mt-[16px]">
+        <label className="mb-[8px]">تاريخ الميلاد:</label>
+        <input
+          className="border h-[48px]  bg-slate-100 w-full p-[4px] rounded"
+          placeholder="تاريخ الميلاد"
+          type="date"
+        />
+      </div>
+      <div className="justify-start mt-[16px]">
+        <label className="mb-[8px]">الجنس:</label>
+        <div className="flex justify-between gap-[16px]">
+          <button type="button" onClick={()=>{setPatient(prev=>({...prev,gender:'male'}))}} className={`border h-[40px] ${patient.gender=='male'?"bg-[#74ABE2] text-white font-bold":"bg-slate-100 text-slate-400"} rounded p-[8px] w-full  `}>
+            ذكر
+          </button>
+          <button type="button" onClick={()=>{setPatient(prev=>({...prev,gender:'female'}))}} className={`border h-[40px] ${patient.gender=='female'?"bg-[#E274CE] text-white font-bold":"bg-slate-100 text-slate-400"} rounded p-[8px] w-full `}>
+            انثى
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      </div>
+      <div className="justify-start mt-[16px]">
+        <label className="mb-[8px]">سبب التنويم:</label>
+        <select
+        required
+          className="border h-[48px]  bg-slate-100 w-full p-[4px] rounded"
+          placeholder="سبب التنويم"
+          type="date"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <option value="">سبب التنويم</option>
+          <option>جراحة</option>
+          <option>مرض معدي</option>
+          <option>مرض غير معدي</option>
+          <option>طوارئ</option>
+        </select>
+      </div>
+      <div className="justify-start mt-[16px]">
+        <label className="mb-[8px]">ملاحظات الطبيب المسؤول:</label>
+        <textarea
+          className="border h-[96px]  bg-slate-100 w-full p-[4px] rounded"
+          placeholder="ملاحظات الطبيب المسؤول"
+        />
+      </div>
+      <div className="justify-start mt-[16px]">
+        <label className="mb-[8px]">التاريخ المرضي:</label>
+        <textarea
+          className="border h-[96px]  bg-slate-100 w-full p-[4px] rounded"
+          placeholder="التاريخ المرضي"
+        />
+      </div>
+      <div className=" mt-[16px]">
+        <label className="mb-[8px] w-full">أدوية يتناولها المريض:</label>
+        <br />
+        <small className="w-full text-slate-500">(فترة ٦ اشهر سابقة)</small>
+        <textarea
+          className="border h-[96px]  bg-slate-100 w-full p-[4px] rounded"
+          placeholder="أدوية يتناولها المريض"
+        />
+      </div>
+      <div className="justify-start mt-[16px]">
+        <label className="mb-[8px]">تاريخ الخروج:</label>
+        <div className="flex justify-between gap-[16px]">
+        <input
+          className="border h-[48px]  bg-slate-100 w-full p-[4px] rounded"
+          placeholder="تاريخ الخروج"
+          type="date"
+        />
+        <input
+          className="border h-[48px]  bg-slate-100 w-full p-[4px] rounded"
+          placeholder="تاريخ الخروج"
+          type="time"
+        />
+        </div>
+      </div>
+      <div className="flex justify-center mt-[32px]">
+        <button className="bg-[#488CD0] text-white p-[8px] w-full max-w-[300px] rounded font-bold"> تسجيل</button>
+        </div>
+        
     </div>
+    </form>
   );
 }
